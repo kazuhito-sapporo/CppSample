a70c4f9cb8004b2acb8ebe6998571cc431b3200b
kazuhito-sapporo <tech@km-office.zombie.JP>
2025-07-27 21:56:48 +0900

    プログラム追加

---
+++ kuku.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 9; i++){
+        for (int j = 1; j <= 9; j++)
+            cout << setw(3) << i * j;
+        cout << '\n';  
+    }
+}
