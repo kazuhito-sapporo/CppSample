793fb94e715961ba573df2edf6690b2881934341
kazuhito-sapporo <tech@km-office.zombie.JP>
2025-07-27 20:59:00 +0900

    初回コミット: .gitignoreなどを追加

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+// HelloWorld.cpp
+// This program prints "Hello, World!" to the console.
+// It uses the iostream library for input and output operations.
+// The main function is the entry point of the program.
+// The program returns 0 to indicate successful execution.
+// The 'using namespace std;' line allows us to use standard library features without prefixing them with 'std::'.
+// This is a basic example to demonstrate the structure of a C++ program
+// C++17 language standard is used for this project.e
